b7eaafbb73b313145174ad7e87c7ae150128196e
kastashenkova <astashenkova.katya@gmail.com>
2026-01-03 12:00:36 +0200

    fixed pagination

--- src/main/java/mate/academy/controller/BookController.java
+++ src/main/java/mate/academy/controller/BookController.java
@@ -6,7 +6,6 @@ import mate.academy.dto.BookDto;
 import mate.academy.dto.CreateBookRequestDto;
 import mate.academy.repository.book.BookSearchParameters;
 import mate.academy.service.BookService;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -54,7 +53,7 @@ public class BookController {
     }
 
     @GetMapping("/search")
-    public Page<BookDto> search(BookSearchParameters searchParameters, Pageable pageable) {
+    public List<BookDto> search(BookSearchParameters searchParameters, Pageable pageable) {
         return bookService.search(searchParameters, pageable);
     }
 }
--- src/main/java/mate/academy/service/BookService.java
+++ src/main/java/mate/academy/service/BookService.java
@@ -4,7 +4,6 @@ import java.util.List;
 import mate.academy.dto.BookDto;
 import mate.academy.dto.CreateBookRequestDto;
 import mate.academy.repository.book.BookSearchParameters;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface BookService {
@@ -18,5 +17,5 @@ public interface BookService {
 
     void deleteBookById(Long id);
 
-    Page<BookDto> search(BookSearchParameters searchParameters, Pageable pageable);
+    List<BookDto> search(BookSearchParameters searchParameters, Pageable pageable);
 }
