09593bfc58e30769196c3d5308913f84331a674e
kastashenkova <astashenkova.katya@gmail.com>
2026-01-07 02:19:32 +0200

    fixed details

--- src/main/java/mate/academy/repository/cart/ShoppingCartRepository.java
+++ src/main/java/mate/academy/repository/cart/ShoppingCartRepository.java
@@ -2,15 +2,12 @@ package mate.academy.repository.cart;
 
 import java.util.Optional;
 import mate.academy.model.cart.ShoppingCart;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface ShoppingCartRepository
         extends JpaRepository<ShoppingCart, Long> {
 
-    @Query("SELECT sc FROM ShoppingCart sc"
-            + " LEFT JOIN FETCH sc.cartItems ci"
-            + " LEFT JOIN FETCH ci.book"
-            + " WHERE sc.user.id = :id AND sc.isDeleted = false")
+    @EntityGraph(attributePaths = {"cartItems", "cartItems.book"})
     Optional<ShoppingCart> findByUserId(Long id);
 }
